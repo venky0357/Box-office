c7929e8ffbda5c5383bb8200b4fa86397327ff88
Venkatesh <107678214+venky0357@users.noreply.github.com>
2023-05-27 14:30:23 +0530

    added tv api search

--- OneDrive/Desktop/Intershala/box-office/src/pages/HomePage.jsx
+++ OneDrive/Desktop/Intershala/box-office/src/pages/HomePage.jsx
@@ -1,8 +1,23 @@
-import {Link} from 'react-router-dom'
+import { useState } from "react";
 const HomePage =()=>{
+  const [searchInput,setString]=useState('');
+
+    const handlechange=(ev)=>{
+      const val=ev.target.value;
+      setString(val);
+    } 
+    const onsearch=async (ev)=>{
+         ev.preventDefault();
+        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchInput}`);
+        const body= await  response.json();
+        console.log(body);
+    }
     return (
         <div>
-            <Link to='/starred'>go to Starred page</Link>
+        <form onSubmit={onsearch}>
+            <input type="text" value={searchInput} onChange={handlechange}/>
+            <button type="submit" >Search</button>
+        </form>
         </div>
     );
 }
